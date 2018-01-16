a8438148bb808fa20b4e6cb812f0af18b5aaaefe
NB_Work\Rogerio <rogerio.dasilva@formstack.com>
2018-01-15 22:46:45 -0500

    Add project files.

---
+++ BlueTooth_Connector_Arduino/Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace BlueTooth_Connector_Arduino
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtMessage = new System.Windows.Forms.TextBox();
+            this.btnRefresh = new System.Windows.Forms.Button();
+            this.cbListDevices = new System.Windows.Forms.ComboBox();
+            this.btnSendMessage = new System.Windows.Forms.Button();
+            this.lblLog = new System.Windows.Forms.Label();
+            this.lblError = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtMessage
+            // 
+            this.txtMessage.Location = new System.Drawing.Point(33, 87);
+            this.txtMessage.Name = "txtMessage";
+            this.txtMessage.Size = new System.Drawing.Size(238, 20);
+            this.txtMessage.TabIndex = 0;
+            // 
+            // btnRefresh
+            // 
+            this.btnRefresh.Location = new System.Drawing.Point(33, 12);
+            this.btnRefresh.Name = "btnRefresh";
+            this.btnRefresh.Size = new System.Drawing.Size(75, 23);
+            this.btnRefresh.TabIndex = 1;
+            this.btnRefresh.Text = "Refresh";
+            this.btnRefresh.UseVisualStyleBackColor = true;
+            this.btnRefresh.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // cbListDevices
+            // 
+            this.cbListDevices.FormattingEnabled = true;
+            this.cbListDevices.Location = new System.Drawing.Point(132, 14);
+            this.cbListDevices.Name = "cbListDevices";
+            this.cbListDevices.Size = new System.Drawing.Size(246, 21);
+            this.cbListDevices.TabIndex = 4;
+            // 
+            // btnSendMessage
+            // 
+            this.btnSendMessage.Location = new System.Drawing.Point(277, 87);
+            this.btnSendMessage.Name = "btnSendMessage";
+            this.btnSendMessage.Size = new System.Drawing.Size(101, 23);
+            this.btnSendMessage.TabIndex = 5;
+            this.btnSendMessage.Text = "Send Message";
+            this.btnSendMessage.UseVisualStyleBackColor = true;
+            this.btnSendMessage.Click += new System.EventHandler(this.btnSendMessage_Click);
+            // 
+            // lblLog
+            // 
+            this.lblLog.AutoSize = true;
+            this.lblLog.Location = new System.Drawing.Point(33, 140);
+            this.lblLog.Name = "lblLog";
+            this.lblLog.Size = new System.Drawing.Size(25, 13);
+            this.lblLog.TabIndex = 6;
+            this.lblLog.Text = "Log";
+            // 
+            // lblError
+            // 
+            this.lblError.AutoSize = true;
+            this.lblError.Location = new System.Drawing.Point(30, 239);
+            this.lblError.Name = "lblError";
+            this.lblError.Size = new System.Drawing.Size(25, 13);
+            this.lblError.TabIndex = 7;
+            this.lblError.Text = "Log";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(402, 272);
+            this.Controls.Add(this.lblError);
+            this.Controls.Add(this.lblLog);
+            this.Controls.Add(this.btnSendMessage);
+            this.Controls.Add(this.cbListDevices);
+            this.Controls.Add(this.btnRefresh);
+            this.Controls.Add(this.txtMessage);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtMessage;
+        private System.Windows.Forms.Button btnRefresh;
+        private System.Windows.Forms.ComboBox cbListDevices;
+        private System.Windows.Forms.Button btnSendMessage;
+        private System.Windows.Forms.Label lblLog;
+        private System.Windows.Forms.Label lblError;
+    }
+}
+
---
+++ BlueTooth_Connector_Arduino/Form1.cs
@@ -0,0 +1,143 @@
+﻿using InTheHand.Net;
+using InTheHand.Net.Bluetooth;
+using InTheHand.Net.Sockets;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BlueTooth_Connector_Arduino
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            List<string> objListDevices = new List<string>();
+
+            BluetoothDeviceInfo[] devices;
+            using (BluetoothClient sdp = new BluetoothClient())
+            {
+                devices = sdp.DiscoverDevices();
+            }
+
+            
+            foreach (BluetoothDeviceInfo deviceInfo in devices)
+            {
+                //TODO: Remove it
+                if (deviceInfo.DeviceName.IndexOf("Rogerio") == -1)
+                    continue;
+
+                deviceInfo.Refresh();
+                objListDevices.Add(deviceInfo.DeviceName + " - " + deviceInfo.DeviceAddress);
+
+                //foreach (Guid pService in deviceInfo.InstalledServices)
+                //{
+                //    BluetoothListener objListener = new BluetoothListener(pService);
+
+                //    BluetoothEndPoint ep = new BluetoothEndPoint(deviceInfo.DeviceAddress, pService);
+                //    BluetoothClient cli = new BluetoothClient(ep);
+                //}
+
+                //this works
+                //BluetoothSecurity.RemoveDevice(deviceInfo.DeviceAddress);
+                //BluetoothSecurity.SetPin(deviceInfo.DeviceAddress, "0000");
+            }
+
+            cbListDevices.Items.Clear();
+
+            foreach (string pDevice in objListDevices)
+            {
+                cbListDevices.Items.Add(pDevice);
+            }
+
+            if (cbListDevices.Items.Count > 0)
+            {
+                cbListDevices.SelectedIndex = 0;
+            }
+        }
+
+        private void SendMessage(string pAddress, string pMessage)
+        {
+            try
+            {
+                BluetoothAddress objDeviceAddress = BluetoothAddress.Parse(pAddress);
+
+                BluetoothDeviceInfo objDevice = new BluetoothDeviceInfo(objDeviceAddress);
+
+                if (objDevice.Remembered && !objDevice.Connected && objDevice.Authenticated)
+                {
+                    BluetoothSecurity.PairRequest(objDevice.DeviceAddress, "0000");
+                }
+                else
+                {
+                    BluetoothSecurity.SetPin(objDevice.DeviceAddress, "0000");
+                }
+
+                Guid serviceClass = BluetoothService.SerialPort;
+                BluetoothEndPoint ep = new BluetoothEndPoint(objDeviceAddress, serviceClass);
+
+                BluetoothClient cli = new BluetoothClient();
+                cli.Connect(ep);
+                Stream peerStream = cli.GetStream();
+
+                byte[] bMessage = System.Text.Encoding.ASCII.GetBytes(pMessage);
+                peerStream.Write(bMessage, 0, bMessage.Length);
+
+                peerStream.Close();
+                cli.Close();
+            }
+            catch (SocketException ex)
+            {
+                string reason;
+                switch (ex.ErrorCode)
+                {
+                    case 10048: // SocketError.AddressAlreadyInUse
+                        // RFCOMM only allow _one_ connection to a remote service from each device.
+                        reason = "There is an existing connection to the remote Chat2 Service";
+                        break;
+                    case 10049: // SocketError.AddressNotAvailable
+                        reason = "Chat2 Service not running on remote device";
+                        break;
+                    case 10064: // SocketError.HostDown
+                        reason = "Chat2 Service not using RFCOMM (huh!!!)";
+                        break;
+                    case 10013: // SocketError.AccessDenied:
+                        reason = "Authentication required";
+                        break;
+                    case 10060: // SocketError.TimedOut:
+                        reason = "Timed-out";
+                        break;
+                    default:
+                        reason = null;
+                        break;
+
+                }
+
+                lblError.Text = reason;
+            }
+            catch (Exception ex)
+            {
+                lblError.Text = "Error unexpected! Ex[" + ex.Message + "]";
+            }
+
+        }
+
+        private void btnSendMessage_Click(object sender, EventArgs e)
+        {
+            string pItem = cbListDevices.SelectedItem.ToString();
+            SendMessage(pItem.Split('-')[1].Trim(), txtMessage.Text);
+        }
+    }
+}
